98ede86c4365a69be7a8a009ac30b88af0cda137
SinthanaPrabakaran <Sinthana.2023@vitstudent.ac.in>
2025-07-04 00:13:24 +0530

    login and logout

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,8 @@ const wishlistRoutes = require("./routes/wishlistRoutes");
 const cartRoutes = require("./routes/cartRoutes");
 const Cart = require("./models/Cart");
 const Wishlist = require("./models/Wishlist");
+const Customer = require("./models/Customer");
+
 
 dotenv.config();
 const app = express();
@@ -59,6 +61,34 @@ app.use("/cart", cartRoutes);
 
 
 // ORIGINALL HOMEEEE
+
+// Middleware to get cart and wishlist counts and user info for all routes
+app.use(async (req, res, next) => {
+  try {
+    let cartCount = 0;
+    let wishlistCount = 0;
+    let user = null;
+    
+    if (req.session.userId) {
+      cartCount = await Cart.countDocuments({ userId: req.session.userId });
+      wishlistCount = await Wishlist.countDocuments({ userId: req.session.userId });
+      
+      // Get user information if logged in
+      if (req.session.userRole === "customer") {
+        user = await Customer.findById(req.session.userId).select("fullName email");
+      }
+    }
+    
+    res.locals.cartCount = cartCount;
+    res.locals.wishlistCount = wishlistCount;
+    res.locals.user = user;
+    res.locals.isLoggedIn = !!req.session.userId;
+    next();
+  } catch (err) {
+    console.error("Error getting counts or user info:", err);
+    next();
+  }
+});
 
 app.get("/", async (req, res) => {
   try {
@@ -183,7 +213,17 @@ const User = require("./models/User"); // make sure this is imported
 app.get("/home", async (req, res) => {
   try {
     const products = await Product.find();
-    res.render("home", { products });
+    let user = null;
+
+    if (req.session.userId && req.session.userRole === "customer") {
+      user = await Customer.findById(req.session.userId);
+    }
+
+    res.render("home", {
+      products,
+      isLoggedIn: !!user,
+      user,
+    });
   } catch (err) {
     console.error(err);
     res.status(500).send("Failed to load products");
